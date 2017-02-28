f996a52e1ad256c16710eae652dab073563b29d2
Arya Tavakoli <aryat@sfu.ca>
2017-02-27 19:35:16 -0800

    Implements Journey  Menu buttons

--- app/src/main/java/com/cmpt276/indigo/carbontracker/JourneyMenu.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/JourneyMenu.java
@@ -1,7 +1,10 @@
 package com.cmpt276.indigo.carbontracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JourneyMenu extends AppCompatActivity {
 
@@ -9,7 +12,42 @@ public class JourneyMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journey_menu);
+        //Allows for backbutton
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        transportSelectbtn();
+        routeSelectbtn();
+    }
+
+
+    //Launch Create a Transport Mode activity
+    private void transportSelectbtn() {
+        Button btn = (Button) findViewById(R.id.journey_menu_select_transport_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JourneyMenu.this, TransportationSelectActvitiy.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+    //Go to carbon footprint activity
+    private void routeSelectbtn() {
+        Button btn = (Button) findViewById(R.id.journey_menu_select_route_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JourneyMenu.this, RouteSelectActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
