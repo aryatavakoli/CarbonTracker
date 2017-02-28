541a6911080a95d1ea52a9541f9b7d01727f3ca9
Arya Tavakoli <aryat@sfu.ca>
2017-02-27 19:31:28 -0800

    Journey Menu implements back button

--- app/src/main/java/com/cmpt276/indigo/carbontracker/JourneyMenu.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/JourneyMenu.java
@@ -9,5 +9,7 @@ public class JourneyMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journey_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
