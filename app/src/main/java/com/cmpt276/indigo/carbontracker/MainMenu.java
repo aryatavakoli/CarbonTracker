23f6605dfe834fe482c52003bb9ae244b0d9ac31
Arya Tavakoli <aryat@sfu.ca>
2017-02-28 23:48:23 -0800

    reads CSV file,CSV file has invlaid data causes crash

--- app/src/main/java/com/cmpt276/indigo/carbontracker/MainMenu.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/MainMenu.java
@@ -3,8 +3,19 @@ package com.cmpt276.indigo.carbontracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.awt.font.NumericShaper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.DoubleBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -15,6 +26,47 @@ public class MainMenu extends AppCompatActivity {
 
         carbonFootprintSelectbtn();
         journeySelectbtn();
+        readVehicleData();
+    }
+
+    private List<VehicleData> vehicledata = new ArrayList<>();
+
+    private void readVehicleData() {
+
+        InputStream is = getResources().openRawResource(R.raw.vehicles);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        try {
+            reader.readLine();
+            while ((line = reader.readLine()) != null){
+                //Log.d("MainMenu", "Error on Line: " + line);
+                //split by columns
+                String[] token = line.split(",");
+                //read the data
+                VehicleData  data = new VehicleData();
+                data.setMake(token[46]);
+                //Log.d("MainMenu", "No Error on SetMake");
+                data.setModel(token[47]);
+                //Log.d("MainMenu", "No Error on SetModel");
+                data.setYear(Integer.parseInt(token[63]));
+                //Log.d("MainMenu", "No Error on SetYear");
+                data.setCarbonFootprintGpm(Double.parseDouble(token[14]));
+                //Log.d("MainMenu", "No Error on SetCarbon");
+                vehicledata.add(data);
+
+                Log.d("MainMenu", "Just created: " + "make: " + data.getMake() + ", " + "model:" + data.getModel() + ", " + "Year: " + data.getYear() +", " + "CarbonFootPrint: " + data.getCarbonFootprintGpm());
+            }
+
+        } catch (IOException e){
+            Log.wtf("MainMenu", "Error reading datafile on Line: " + line, e);
+            e.printStackTrace();
+
+        }
+
+
     }
 
     //Launch Create a jounrney activity
---
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/VehicleData.java
@@ -0,0 +1,54 @@
+package com.cmpt276.indigo.carbontracker;
+
+/**
+ * Created by arya on 27/02/17.
+ */
+
+public class VehicleData {
+    private String make;
+    private String model;
+    private int year;
+    private double carbonFootprintGpm;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getCarbonFootprintGpm() {
+        return carbonFootprintGpm;
+    }
+
+    public void setCarbonFootprintGpm(double carbonFootprintGpm) {
+        this.carbonFootprintGpm = carbonFootprintGpm;
+    }
+
+    @Override
+    public String toString() {
+        return "VehicleData{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", carbonFootprintGpm=" + carbonFootprintGpm +
+                '}';
+    }
+}
