30b1b7758efdd3a9fef60a3b3e4707986f5180af
Arya Tavakoli <aryat@sfu.ca>
2017-03-05 16:58:48 -0800

    Major Changes to Carbonfootprint
    -moves everything into fragments
    -implements tab view with graph and table

--- app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintGraphTab.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintGraphTab.java
@@ -1,43 +1,33 @@
 package com.cmpt276.indigo.carbontracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class CarbonFootprintGraph extends AppCompatActivity {
+/**
+ * Created by arya on 05/03/17.
+ */
 
+public class CarbonFootprintGraphTab extends Fragment {
     float carbonfootprint[] = {10.0f, 20.0f ,30.0f, 40.0f,50.0f,60.0f ,43.0f};
 
     String journeyName[] = {"Journey 1", "Journey 2" , "Journey 3","Journey 4","Journey 5", "Journey 6","Journey 7"};
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_carbon_footprint_graph);
-        //Allows for backbutton
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        setupPieGraph();
-    }
-
-    private void setupPieGraph() {
-        PieChart pieChart = (PieChart) findViewById(R.id.bar_graph);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_carbon_footprint_graph, container, false);
+        PieChart pieChart = (PieChart) rootView.findViewById(R.id.bar_graph);
         ArrayList<String> pieEntrieslabels = new ArrayList<>();
         ArrayList<PieEntry> pieEntries = new ArrayList<>();
 
@@ -46,14 +36,14 @@ public class CarbonFootprintGraph extends AppCompatActivity {
             pieEntries.add(new PieEntry(carbonfootprint[i],journeyName[i]));
         }
 
-
         PieDataSet dataSets = new PieDataSet(pieEntries,null);
         PieData data = new PieData(dataSets);
         dataSets.setColors(ColorTemplate.VORDIPLOM_COLORS);
         pieChart.setDescription(null);
         pieChart.setData(data);
         pieChart.getLegend().setEnabled(false);
-        pieChart.animateY(3000);
+        pieChart.animateY(2000);
         pieChart.invalidate();
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintTableTab.java
@@ -0,0 +1,30 @@
+package com.cmpt276.indigo.carbontracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.cmpt276.indigo.carbontracker.carbon_tracker_model.FuelDataInputStream;
+
+/**
+ * Created by arya on 05/03/17.
+ */
+
+public class CarbonFootprintTableTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_carbon_footprint_table, container, false);
+
+        ListView tableListView = (ListView) rootView.findViewById(R.id.list);
+
+        TableAdapter adapter = new TableAdapter(getActivity(), FuelDataInputStream.getSampleData());
+
+        tableListView.setAdapter(adapter);
+        return rootView;
+    }
+}
