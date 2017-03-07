7ecf2948faca5e5dc035737f4c34671a1effb18a
Arya Tavakoli <aryat@sfu.ca>
2017-03-06 20:20:23 -0800

    small UI change to graph

--- app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintGraphTab.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintGraphTab.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cmpt276.indigo.carbontracker.carbon_tracker_model.CarbonFootprintComponentCollection;
+import com.cmpt276.indigo.carbontracker.carbon_tracker_model.JourneyModel;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -14,6 +15,7 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by arya on 05/03/17.
@@ -39,7 +41,7 @@ public class CarbonFootprintGraphTab extends Fragment {
 
         PieDataSet dataSets = new PieDataSet(pieEntries,null);
         PieData data = new PieData(dataSets);
-        dataSets.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        dataSets.setColors(ColorTemplate.LIBERTY_COLORS);
         pieChart.setDescription(null);
         pieChart.setData(data);
         pieChart.getLegend().setEnabled(false);
