b42cd048f5cb9ae1c526f4d409be53908f833b5a
Arya Tavakoli <aryat@sfu.ca>
2017-03-22 10:38:40 -0700

    - Small UI changes addUtility

--- app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintDailyTab.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/CarbonFootprintDailyTab.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CarbonFootprintDailyTab extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.activity_carbon_footprint_daily_tab, container, false);
         return rootView;
     }
