7e6c3d649073dabd23dfa5c867fabf32c5f41eea
Faranak Nobakhtian <fnobakht@sfu.ca>
2017-03-28 19:49:39 -0700

    Added side navigation to journey select activity.

--- app/src/main/java/com/cmpt276/indigo/carbontracker/JourneySelectActivity.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/JourneySelectActivity.java
@@ -3,14 +3,23 @@ package com.cmpt276.indigo.carbontracker;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.cmpt276.indigo.carbontracker.carbon_tracker_model.CarbonFootprintComponentCollection;
 import com.cmpt276.indigo.carbontracker.carbon_tracker_model.JourneyModel;
@@ -24,7 +33,7 @@ import java.util.List;
 /**
  * the activity to display all the journey into a listview
  */
-public class JourneySelectActivity extends AppCompatActivity {
+public class JourneySelectActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     ArrayList<JourneyModel> journies;
     ArrayList<VehicleModel> vehicles;
     ArrayList<RouteModel> routes;
@@ -41,27 +50,36 @@ public class JourneySelectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.toolbar_journey_select);
+        setContentView(R.layout.activity_journey_select);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        setupBottomNavigation();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
         carbonFootprintInterface = CarbonFootprintComponentCollection.getInstance();
-        startAddActivity();
+//        startAddActivity();
         createListView();
         setupEditVehicleLongPress();
     }
 
-    private void startAddActivity() { //launching the add activity
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(JourneySelectActivity.this, JourneyAddActivity.class);
-                startActivityForResult(intent, ACTIVITY_RESULT_ADD);
-            }
-        });
-    }
+//    private void startAddActivity() { //launching the add activity
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(JourneySelectActivity.this, JourneyAddActivity.class);
+//                startActivityForResult(intent, ACTIVITY_RESULT_ADD);
+//            }
+//        });
+//    }
 
     private void setupList(){
         Resources res = getResources();
@@ -88,6 +106,31 @@ public class JourneySelectActivity extends AppCompatActivity {
             titles[i] = items[i].getText1();
         }
         return titles;
+    }
+
+    private void setupBottomNavigation(){
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                Intent intent = new Intent(JourneySelectActivity.this, JourneyAddActivity.class);
+                switch(item.getItemId())
+                {
+                    case R.id.action_add:
+                        startActivityForResult(intent, ACTIVITY_RESULT_ADD);
+                        Toast.makeText(JourneySelectActivity.this, "Add", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_edit:
+                        startActivityForResult(intent, ACTIVITY_RESULT_EDIT);
+                        Toast.makeText(JourneySelectActivity.this, "Edit", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_remove:
+                        Toast.makeText(JourneySelectActivity.this, "Remove", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     private void createListView() {
@@ -167,5 +210,53 @@ public class JourneySelectActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
