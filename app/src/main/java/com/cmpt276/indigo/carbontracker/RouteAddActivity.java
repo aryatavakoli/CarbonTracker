18419293887b661b7106540b816e732558206c2f
Faranak Nobakhtian <fnobakht@sfu.ca>
2017-03-29 19:33:13 -0700

    Added side and bottom navigation to routeAddActivity class.

--- app/src/main/java/com/cmpt276/indigo/carbontracker/RouteAddActivity.java
+++ app/src/main/java/com/cmpt276/indigo/carbontracker/RouteAddActivity.java
@@ -5,11 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,7 +38,7 @@ import static java.sql.Types.NULL;
     implements Route Add activity
  */
 
-public class RouteAddActivity extends AppCompatActivity {
+public class RouteAddActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     public static final int RESULT_DELETE = 12;
     CarbonFootprintComponentCollection carbonFootprintInterface;
     private boolean editing = false;
@@ -40,15 +49,52 @@ public class RouteAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_add);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
         carbonFootprintInterface = CarbonFootprintComponentCollection.getInstance();
         populateUIFromIntent();
         calculate();
         recalculateOnChange(R.id.add_route_editText_city_distance);
         recalculateOnChange(R.id.add_route_editText_highway_distance);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         setupOKButton();
         setupDeleteButton();
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation(){
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                Intent intent = new Intent(RouteAddActivity.this, RouteSelectActivity.class);
+                switch(item.getItemId())
+                {
+                    case R.id.action_add:
+                        break;
+                    case R.id.action_edit:
+                        break;
+                    case R.id.action_remove:
+                        break;
+                }
+                return true;
+            }
+        });
+        if(editing) {
+            Menu menu = bottomNavigationView.getMenu();
+            MenuItem addItem = menu.findItem(R.id.action_add);
+            addItem.setTitle("Update");
+            addItem.setIcon(R.drawable.ic_update);
+        }
     }
 
     private void populateUIFromIntent() { //If the user wants to edit the pot this function gets the value from the
@@ -234,6 +280,56 @@ public class RouteAddActivity extends AppCompatActivity {
         Intent intent = makeIntentForNewRoute(packageContext);
         intent.putExtra("route", route);
         return intent;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
